51b65e36636e790edc6593c4e9abf6e1289c4de8
JuanTA97 <juppalaciope@unal.edu.co>
2025-05-03 10:14:08 -0500

    _internals

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,15 +1,7 @@
 # obtain a list of files in the input directory
 import os
 
-
-def read_all_lines():
-    all_lines = []
-    input_file_list = os.listdir("data/input/")
-    for filename in input_file_list:
-        with open(filename, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-            all_lines.extend(lines)
-    return all_lines
+from ._internals.write_count_words import write_count_words
 
 
 def main():
@@ -36,18 +28,6 @@ def main():
     ##
     # create the directory output/ if it doesn't exist
     write_count_words(counter)
-
-
-def write_count_words(counter):
-    if not os.path.exists("data/output"):
-        os.makedirs("data/output")
-
-    ##
-    # save the results using tsv format
-    with open("data/output/results.tsv", "w", encoding="utf-8") as f:
-        for key, value in counter.items():
-            # write the key and vlue to tahe file
-            f.write(f"{key}\t{value}\n")
 
 
 if __name__ == "__main__":
